9caa4f8f1c042526a6400634b0426d0e4be2a8c2
Héloïse <heloise.lafontaine@gmail.com>
2024-12-17 11:29:27 +0100

    correction erreur vite config : import fileURLToPath

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Définir __dirname pour les modules ES
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
