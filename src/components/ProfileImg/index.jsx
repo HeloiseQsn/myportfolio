2e9363c809a1bf939760969eaad4e735bf23ed10
Héloïse <heloise.lafontaine@gmail.com>
2024-12-17 11:16:49 +0100

    ajout style photo profil

--- src/components/ProfileImg/index.jsx
+++ src/components/ProfileImg/index.jsx
@@ -1,9 +1,14 @@
+import './profileImg.scss'
 import profileImg from '../../assets/images/profil/profil.webp'
 
 function ProfileImage() {
   return (
     <div className="profile-img__container">
-      <img src={profileImg} alt="Profile" className="profile-image" />
+      <img
+        src={profileImg}
+        alt="Profile"
+        className="profile-img__container--photo"
+      />
     </div>
   )
 }
