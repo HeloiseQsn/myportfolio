92d39dd7f0a65d4f449f93bd78e4c02b6537d964
Héloïse <heloise.lafontaine@gmail.com>
2024-12-17 11:27:29 +0100

    ajout style photo et title dans page home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,16 @@
+import './home.scss'
 import ProfileImage from '../../components/ProfileImg'
 import Title from '../../components/Title'
 
 function Home() {
   return (
-    <div className="mainHome">
+    <main className="main-home">
       <ProfileImage />
       <Title />
       <h2>Mes projets</h2>
       <h2>Qui suis-je ? </h2>
       <h2>Mes expériences</h2>
-    </div>
+    </main>
   )
 }
 
