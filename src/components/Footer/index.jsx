af4b137a413aac585f4f6e7d5b552178e86b5351
Héloïse <heloise.lafontaine@gmail.com>
2024-12-20 10:24:18 +0100

    ajout footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,15 @@
 import './footer.scss'
+import Linkedin from '../../assets/images/linkedin.webp'
 
 function Footer() {
   return (
     <footer>
-      <div>footer à compléter</div>
+      <div>Retour haut de page</div>
+      <div className="Linkedinlink">
+        <a href="https://www.linkedin.com/in/h%C3%A9lo%C3%AFse-quinson-b5b5587b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+          <img src={Linkedin} alt="lien vers Linkedin" />
+        </a>
+      </div>
     </footer>
   )
 }
