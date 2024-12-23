a2197646720036b518899bfe8f93ed5787a5f7b6
Héloïse <heloise.lafontaine@gmail.com>
2024-12-23 11:45:25 +0100

    ajout routes pages project et contact pour gestion breakpoint mobile

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from '../pages/home'
+import Contact from '../pages/contact'
+import Project from '../pages/project'
 import Error from '../pages/error'
 
 function RoutesPortfolio() {
@@ -8,6 +10,8 @@ function RoutesPortfolio() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
